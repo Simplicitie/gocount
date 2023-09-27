3afee282a86d27fbfbe879d58e5e62b2b7cdbf89
Michael Lord <30150685+Simplicitie@users.noreply.github.com>
2023-09-26 20:24:18 -0400

    add REST interface endpoints

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
 )
+
+var client *redis.Client
+var ctx context.Context
 
 func main() {
 	// Example useage: REDIS_HOST=localhost REDIS_PORT=6379 go run main.go
@@ -22,30 +26,44 @@ func main() {
 		port = "6379"
 	}
 
+	// establish connection to the Redis instence
 	connection_string := fmt.Sprintf("redis://%s:%s@%s:%s/1", uname, pass, host, port)
 	opt, err := redis.ParseURL(connection_string)
 	if err != nil {
 		panic(err)
 	}
-	client := redis.NewClient(opt)
+	client = redis.NewClient(opt)
 
-	ctx := context.Background()
+	ctx = context.Background()
 
+	// Set the initial count to 0
 	err = client.Set(ctx, "count", 0, 0).Err()
 	if err != nil {
 		panic(err)
 	}
 
+	// Start the Gin web Server
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Welcome to my counter!\n  To check the current count navigate to /count\n  To add one to the number, navigate to /incr")
+	})
+	r.GET("/incr", incrCountByOne)
+	r.GET("/count", getCount)
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func getCount(c *gin.Context) {
 	val, err := client.Get(ctx, "count").Result()
 	if err != nil {
-		panic(err)
+		c.Error(err)
 	}
-	fmt.Println("foo", val)
+	c.String(200, fmt.Sprintln("Current count:", val))
+}
 
+func incrCountByOne(c *gin.Context) {
 	count, err := client.Incr(ctx, "count").Result()
 	if err != nil {
-		panic(err)
+		c.Error(err)
 	}
-	fmt.Println("foo", count)
-
+	c.String(200, fmt.Sprintln("Count incremented by one and is now:", count))
 }
